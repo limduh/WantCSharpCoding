7b030b52b1ab8a5de28a3915414f93672f090293
limduhyoung <limduh2@naver.com>
2024-04-05 11:17:35 +0900

    class3추가
    추가class3!!

---
+++ WantCSharpCoding/Class3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WantCSharpCoding
+{
+    internal class Class3
+    {
+        public class Example
+        {
+            public static void Main()
+            {
+                String s;
+
+                Console.Clear();
+                s = Console.ReadLine();
+
+                Console.WriteLine(s);
+            }
+        }
+
+    }
+}
