57b64197ab83a4a3e553e6df041de67d679a9ad9
limduhyoung <limduh2@naver.com>
2024-04-05 10:48:56 +0900

    초기 커밋.

---
+++ WantCSharpCoding/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WantCSharpCoding
+{
+    internal class Class1
+    {
+
+        /*public static void Main()
+        {
+            String s;
+
+            Console.Clear();
+            s = Console.ReadLine();
+
+            Console.WriteLine(s);
+        }
+        */
+    }
+
+}
---
+++ WantCSharpCoding/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WantCSharpCoding
+{
+    internal class Class2
+    {
+
+
+        public static void Main()
+        {
+            String s;
+
+            Console.Clear();
+            s = Console.ReadLine();
+
+            Console.WriteLine(s);
+        }
+    }
+}
